ac7216aab0e1bef14f2b1c5808ba74acfbb82d4d
ogrod0707 <ogrod0808@gmail.com>
2022-03-11 19:41:00 +0100

    added some logic to queries and added autoload function

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -12,20 +12,8 @@
             <?php
 
 
-//
-//          $result_set = User::find_all_users();
-//
-//          while($row = mysqli_fetch_array($result_set)){
-//              echo $row['username'] . "<br>";
-//          }
-
-
-          $found_user = User::find_user_by_id(1);
-          $user = User::instantation($found_user);
-
-            echo $user->username;
-
-
+            $found_user = User::find_user_by_id(1);
+            echo $found_user->username;
 
 
             ?>
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -18,35 +18,47 @@ class User{
 
     public static function find_user_by_id($user_id){
         global $database;
-        $result_set =  self::find_this_query("SELECT * FROM users where id = $user_id LIMIT 1");
-        $found_user = mysqli_fetch_array($result_set);
+        $the_result_array =  self::find_this_query("SELECT * FROM users where id = $user_id LIMIT 1");
+
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+
         return $found_user;
     }
 
     public static function find_this_query($sql){
         global $database;
         $result_set = $database->query($sql);
-        return $result_set;
+        $the_object_array = array();
+
+        while($row = mysqli_fetch_array($result_set)){
+            $the_object_array[] = self::instantation($row);
+        }
+        return $the_object_array;
     }
+
+
 
     public static function instantation($the_record){
         $the_object = new self;
 
-//        $the_object->id = $found_user['id'];
-//        $the_object->username = $found_user['username'];
-//        $the_object->password = $found_user['password'];
-//        $the_object->first_name = $found_user['first_name'];
-//        $the_object->last_name = $found_user['last_name'];
-
         foreach ($the_record as $the_attribute => $value){
 
             if($the_object->has_the_attribute($the_attribute))
-                $the_object->the_attribute = $value;
+            {
+                $the_object->$the_attribute = $value;
+            }
+
         }
 
         return $the_object;
     }
 
+private function has_the_attribute($the_attribute){
 
+        $object_properties = get_object_vars($this);
+
+        return array_key_exists($the_attribute, $object_properties);
+
+}
 
 }
